3fce15172fed7e5ab02f0e8694b2d300cfc8d775
Samuel Simeon <simeonlekan0@gmail.com>
2025-04-19 12:36:06 +0100

    changed to bycrypy.js

--- models/user.js
+++ models/user.js
@@ -1,43 +1,40 @@
 // models/User.js
 import mongoose from "mongoose";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";  // switch to bcryptjs
 
-// This tells MongoDB what our "User" data looks like
 const UserSchema = new mongoose.Schema({
   username: {
     type: String,
     required: true,
-    unique: true // No two users can have the same username
+    unique: true, // No two users can have the same username
   },
   email: {
     type: String,
     required: true,
-    unique: true // No two users can have the same email
+    unique: true, // No two users can have the same email
   },
   password: {
     type: String,
-    required: true
-  }
+    required: true,
+  },
 });
 
-// This part automatically scrambles (hashes) the password before saving it
-UserSchema.pre("save", async function (next) {
+// Hash password before saving
+UserSchema.pre("save", function (next) {
   if (!this.isModified("password")) return next();
+
   try {
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
+    const salt = bcrypt.genSaltSync(10);
+    this.password = bcrypt.hashSync(this.password, salt);
     next();
-  } catch (error) {
-    next(error);
+  } catch (err) {
+    next(err);
   }
 });
 
-// This helps us check if a typed-in password is correct by comparing it to the scrambled one
-UserSchema.methods.comparePassword = async function (plainPassword) {
-  return bcrypt.compare(plainPassword, this.password);
+// Compare plain text password to hashed
+UserSchema.methods.comparePassword = function (plainPassword) {
+  return bcrypt.compareSync(plainPassword, this.password);
 };
 
-// "User" is what we call our model in the database
-const User = mongoose.model("User", UserSchema);
-
-export default User;
+export default mongoose.model("User", UserSchema);
